3d8798bf03b13a3cf2b3dbc523d2dd2221254228
Jason Vanderheyden <jason.vanderheyden@yahoo.com>
2020-01-02 20:52:34 -0500

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Remote Creative`,
+    description: `We are a web development company interested in the future of technology, communication, innovation, and connecting people around the world.`,
+    author: `@theremotecreative`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,13 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `the-remote-creative`,
+        short_name: `remote`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/remote-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
